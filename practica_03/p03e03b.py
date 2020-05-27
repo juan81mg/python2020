b334a203ab89d1b954b64cbdbf44c1f1d07f1e2d
Juan Manuel Gil <juan81mg@gmail.com>
2020-05-26 22:17:24 -0300

    Modificacion ejercicio 3b
    ejercicio terminado, usando lambda

--- practica_03/p03e03b.py
+++ practica_03/p03e03b.py
@@ -12,38 +12,45 @@ jugaron sin recorrer la estructura.
 C - Dada la estructura generada en el ejercicio imprimir el usuario que obtuvo mayor puntaje.
 D - Dada la estructura del ejercicio, ingresar los datos correspondientes a la jugada de un
 usuario. Si el usuario existe previamente, guardar los datos de mayor puntaje.
-Luego imprimir el ranking de los 10 mejores puntajes.
+E - Luego imprimir el ranking de los 10 mejores puntajes.
 Nota: utilizar una expresión lambda para ordenar el diccionario
 """
 
 #usando diccionario de diccionarios
-jugadores = {'fede': { 'nivel': 3, 'puntaje': 4, 'tiempo': 200 }, 'belen': { 'nivel': 5, 'puntaje': 10, 'tiempo': 400 }, 'juan': { 'nivel': 15, 'puntaje': 110, 'tiempo': 400 }, 'pedro': { 'nivel': 5, 'puntaje': 3, 'tiempo': 400 }}
-print(jugadores)
+jugadores = {
+    'fede': {'nivel': 3, 'puntaje': 4, 'tiempo': 200}, 
+    'belen': {'nivel': 5, 'puntaje': 10, 'tiempo': 400}, 
+    'juan': {'nivel': 15, 'puntaje': 110, 'tiempo': 400}, 
+    'pedro': {'nivel': 5, 'puntaje': 3, 'tiempo': 400}
+}
+
 #A
-print(jugadores['pedro'])
+buscado = 'pedro'
+print("Imprimir los datos del jugador buscado: ")
+print(jugadores[buscado] if buscado in jugadores else 'El usuario no existe')
+print()
+
 #B
-print(jugadores.keys())
+print("Nombres de los usuarios que jugaron sin recorrer la estructura:")
+print(list(filter(lambda d:jugadores[d]['tiempo'] != 0, jugadores)))
+print()
 
-print('='*55)
+#C
+print('Jugador con el mayor puntaje: ')
+print(max(jugadores.items(), key=lambda x: x[1]['puntaje']))
+print()
 
-#todos los datos
-print(jugadores.items())
+#D
+partida_nom = input('Ingrese el nombre del Jugador: ')
+partida_puntaje = input('Ingrese el puntaje de la jugada de {}: '.format(partida_nom))
+if partida_nom in jugadores.keys():
+    buscado = jugadores[partida_nom]
+    buscado['puntaje'] = max(buscado['puntaje'], int(partida_puntaje))
+else:
+    print()
+    print('El Jugador no existe')
 
-#recorro la estructura
-for jugador in jugadores.items():
-    print(jugador)
-
-#recorro y accedo al nombre de cada jugador
-for jugador in jugadores.items():
-    print(jugador[0])
-
-#recorro y accedo al nombre de cada jugador por nivel
-for jugador in jugadores.items():
-    print(jugador[1]['nivel'])
-
-#accedo a todos los datos del jugador
-for jugador in jugadores.items():
-    print('El jugador {} tiene los siguientes datos:'.format(jugador[0]))
-    for dato in jugador[1].keys():
-        print('En {} es: {}'.format(dato, jugador[1][dato]))
-
+#E
+jugadores_ord = sorted(jugadores.items(), key=lambda punt: punt[1]['puntaje'], reverse=True)
+print('Ranking TOP 10')
+print(jugadores_ord[:10])
