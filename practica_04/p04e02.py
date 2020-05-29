e41414beb801ce969bab76e928bb4bf6610a6f7b
Juan Manuel Gil <juan81mg@gmail.com>
2020-05-28 22:28:26 -0300

    Update p04e02.py

--- practica_04/p04e02.py
+++ practica_04/p04e02.py
@@ -2,22 +2,17 @@
 2A - Registrar los jugadores del Ejercicio 3 de la Práctica 3 en un archivo
 utilizando cualquiera de las librerías dadas en la teoría (Pickle, JSON,
 CSV). 
-
 2B - Implementar una función denominada modificoDatos, la cual solicita 
 (mediante una interfaz generada con PySimpleGUI) los datos de un
 jugador, si este existe en el archivo, modifica dichos datos en el mismo.
 Si no existe el jugador, lo agrega."""
 
-
 import json
 import PySimpleGUI as sg
-
-
 
 #2B
 def modificoDatos():
     pass
-
 
 #2A
 def guardoDatos(jugadores):
