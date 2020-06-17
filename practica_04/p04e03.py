5d25fe0d531a8590ffd0ee160b11acafd95bc926
Juan Manuel Gil <juan81mg@gmail.com>
2020-06-17 16:19:40 -0300

    Fix archivo de datos

--- practica_04/p04e03.py
+++ practica_04/p04e03.py
@@ -5,7 +5,7 @@ from pattern.es import INFINITIVE, PRESENT, PAST, SG, PL, SUBJUNCTIVE, PERFECTIV
 """Leer un texto desde un archivo y generar uno nuevo (denominado verbos.json) que contenga una estructura con todos los verbos convertidos
 a infinitivo junto con la cantidad de apariciones de cada uno"""
 
-with open("p04e03_datos.txt", "r") as archivo:
+with open("p04e03_Datos.txt", "r") as archivo:
     datos = archivo.read().split(', ')
 
 verbos = []
