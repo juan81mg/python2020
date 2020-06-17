5db81bd7301365f1cd86cc787789d902ad09bdc4
Juan Manuel Gil <juan81mg@gmail.com>
2020-06-17 15:14:44 -0300

    Fix bug setLetra + button.disable()

--- practica_04/p04e09.py
+++ practica_04/p04e09.py
@@ -16,35 +16,8 @@ Se deberán agregar todas las letras disponibles.
 sg.theme('DarkBlue14')
 
 #Comportamiento de los botones
-# definicion clase BotonB para la fila de botones con letras
-class buttonA():
-    def __init__ (self,x,y):
-        self.name = random.choice(string.ascii_letters)
-        self.x = x
-        self.y = y
-        self.state = 1
-        self.disabled = False
-        self.key = (x,y)
-        self.button = sg.Button(self.name, 
-            auto_size_button = False,
-            border_width = 2,
-            disabled = self.disabled,
-            focus = False,
-            key = self.key,
-            pad = (1,1))
-    def update(self):
-        self.button.update(disabled=self.disabled)
-    def getLetra(self):
-        return self.name
-    def enable(self):
-        self.disabled = False 
-        self.update()
-    def disable(self):
-        self.disabled = True
-        self.update()
-
 #deficion de clase BotonA es para el grid de 10x10
-class buttonB():
+class botonA():
     def __init__ (self,x,y):
         self.name = " "
         self.x = x
@@ -53,7 +26,8 @@ class buttonB():
         self.disabled = False
         self.key = (x,y)
         self.button = sg.Button(self.name, 
-            auto_size_button = False,
+            size = (4,2),
+            button_color=('black', 'white'),
             border_width = 2,
             disabled = self.disabled,
             focus = False,
@@ -73,91 +47,111 @@ class buttonB():
         self.disabled = False
         self.update()
 
-# atril con las 5 letras random
-b1 = [[0 for j in range(5)] for i in range(5)]
+# definicion clase BotonB para la fila de botones con letras
+class botonB():
+    def __init__ (self,x,y):
+        self.name = random.choice(string.ascii_letters)
+        self.x = x
+        self.y = y
+        self.state = 1
+        self.disabled = False
+        self.key = (x,y)
+        self.button = sg.Button(self.name, 
+            size = (4,2),
+            button_color=('white', 'black'),
+            border_width = 2,
+            disabled = self.disabled,
+            focus = False,
+            key = self.key,
+            pad = (1,1))
+    def update(self):
+        self.button.update(disabled=self.disabled)
+    def getLetra(self):
+        return self.name
+    def enable(self):
+        self.disabled = False 
+        self.update()
+    def disable(self):
+        self.disabled = True
+        self.update()
 
 # grid de 10x10
-b2 = [[0 for j in range(10)] for i in range(10)]
+ba = [[0 for j in range(10)] for i in range(10)]
 
-# definicion tablero B
-def button1(x,y):
-    #defino la lista con los atributos de buttonB
-    b1[x][y] = buttonA(x,y)
-    # devuelvo el boton
-    return b1[x][y].button
+# atril con las 5 letras random
+bb = [[0 for j in range(5)] for i in range(5)]
 
 # definicion tablero A
-def button2(x, y):
+def buttonA(x, y):
     #defino la lista con los atributos de buttonB
-    b2[x][y] = buttonB(x,y)
+    ba[x][y] = botonA(x,y)
     # devuelvo el boton
-    return b2[x][y].button
+    return ba[x][y].button
 
-# estructura de la ventana
-ventana = [
-    [sg.Text("Tablero B")],
-    [button1(z, 1) for z in range(5)],
-    [sg.Text("Tablero A")] 
-    ] + [
-    [button2(x, y) for x in range(10)] for y in range(10)]
-
-# creo el tablero
-win = sg.Window("Practica_04 | Ejercicio_09", layout=ventana, finalize=True)
+# definicion tablero B
+def buttonB(x,y):
+    #defino la lista con los atributos de buttonB
+    bb[x][y] = botonB(x,y)
+    # devuelvo el boton
+    return bb[x][y].button
 
 # funciones para controlar que no se rompa el programa haciendo click 2 veces en la misma tabla,
-# funcion para deshabilita el valor de cada boton del grid de 10x10
-def disable_A():
-    for i in range(10):
-        for j in range(10):
-            b2[i][j].disable()
+
 
 # funcion para habilitar los boton para guardar la letra
 def enable_A():
     for i in range(10):
         for j in range(10):
             #pregunto el estado del boton para no volver a activar un boton con valor, evita la sobreescritura
-            if (b2[i][j].state == 0):
-                b2[i][j].enable()
+            if (ba[i][j].state == 0):
+                ba[i][j].enable()
+# funcion para deshabilita el valor de cada boton del grid de 10x10
+def disable_A():
+    for i in range(10):
+        for j in range(10):
+            ba[i][j].disable()
 
 #habilita la accion de cada boton del atril
 def enable_B():
     for i in range(5):
-        b1[i][1].enable()
-
+        bb[i][1].enable()
 #deshabilita el comportamiento de cada boton del atril
 def disable_B():
     for i in range(5):
-        b1[i][1].disable()
+        bb[i][1].disable()
 
-# booleano para que no se haga asigne un valor en un casillero lleno
-ok = True
 
+# estructura de la ventana
+tableroA = [[sg.Text('Tablero A')]] + [[buttonA(x, y) for x in range(10)] for y in range(10)]
+tableroB = [[sg.Text('Tablero B')]] + [[buttonB(z, 1) for z in range(5)]]
+layout = tableroA + tableroB
+win = sg.Window("Practica_04 | Ejercicio_09", layout, finalize=True)
+ok=True
+disable_A() # inicializo el tablero A de 10x10 para que no funcione el boton del grid al inicio
 while True:
-    # ejecuto la ventana y guardo los botones y los valores
     event, values = win.read()
-    # si se cierra la ventana, corta
+
     if event is None:
         break
-    # inicializo el tablero A de 10x10
-    disable_A()
-    print(event)
-    x = event[0]
-    y = event[1]
-    # accion de la habilidad
-    if (ok):
 
-        # guardo la letra del atril
-        letra = b1[x][y].getLetra()
-        #control
-        ok = False
-        # habilito para guardar la letra
-        enable_A()
-        # deshabilito en el atril el boton de la letra
-        disable_B()
-    else:
-        # le asigno la letra del atril en el grid
-        b2[x][y].setLetra(letra)
-        #control
-        ok = True
-        # habilito los botones del atril para empezar de nuevo
-        enable_B()
+    disable_A() # inicializo el tablero A de 10x10
+    if (type(event)==str)&(ok):      #control para que no se pise
+        x = int(event[1])
+        y = int(event[4])
+        # print('x >>> ', x)
+        # print('y >>> ', y)
+        letra = bb[x][y].getLetra() # guardo la letra del atril
+        # print('Letra >>> ', letra)
+        enable_A()  # habilito para guardar la letra
+        disable_B()     # deshabilito en el atril
+        ok=False
+    elif (ok==False):
+        x = event[0]
+        y = event[1]
+        print(letra)
+        # print('j >>> ', j, type(j))
+        # print('i >>> ', i)
+        # print('Letra >>> ', letra)
+        ba[x][y].setLetra(letra)  # asigno la letra del atril en el grid
+        enable_B()    # habilito los botones del atril para empezar de nuevo
+        ok=True
